d7e88d8d77cc3b3a418d189214e400f2d8d21e0a
Jonathan Miner <cp_n18@yahoo.com>
2017-10-31 22:12:30 -0400

    cleaned up pylint

--- router/router.py
+++ router/router.py
@@ -1,130 +1,144 @@
 #!/usr/bin/env python
+"""
+Reference design for mesh routing
+"""
 
-import pika
 import threading
 import time
 import hashlib
 import json
 import sys
-
 import importlib
-mesh = importlib.import_module(sys.argv[1], package=None)
+import pika
 
-hash_table = []
-expiration = 60
+MESH = importlib.import_module(sys.argv[1], package=None)
 
-done = False
+HASH_TABLE = []
 
-def in_hash(self,msg):
-    global hash_table
-    m = hashlib.md5()
-    m.update(msg)
-    key = m.digest()
+DONE = False
+
+def in_hash(self, msg):
+    """
+    Check and update the hash table
+    """
+    global HASH_TABLE
+    expiration = 60
+    key = hashlib.md5().update(msg).digest()
     now = time.time()
-    if key in hash_table[self]: 
-        retval = hash_table[self][key] > 0
+    if key in HASH_TABLE[self]:
+        retval = HASH_TABLE[self][key] > 0
     else:
-        hash_table[self][key] = now + expiration 
+        HASH_TABLE[self][key] = now + expiration
         retval = False
-    for key, value in hash_table[self].items():
+    for key, value in HASH_TABLE[self].items():
         if value < now:
-            del hash_table[self][key]
+            del HASH_TABLE[self][key]
     return retval
 
 def get_message(node_id):
+    """
+    Get a message from the queue
+    """
     queue = "q%d" % node_id
     connection = pika.BlockingConnection()
     channel = connection.channel()
     channel.queue_declare(queue=queue)
     messages = []
-    method_frame, properties, body = channel.basic_get(queue) 
+    method_frame, properties, body = channel.basic_get(queue)
     if method_frame:
         messages.append(body)
-        #print "Received: %d %s" % (node_id,body) 
         channel.basic_ack(method_frame.delivery_tag)
     connection.close()
     return messages
 
-def send_message(q,node_id,message):
-    queue = "%s%d" % (q,node_id)
+def send_message(queue_prefix, node_id, message):
+    """
+    Send a Message
+    """
+    queue = "%s%d" % (queue_prefix, node_id)
     connection = pika.BlockingConnection()
     channel = connection.channel()
     channel.queue_declare(queue=queue)
     channel.basic_publish(exchange='',
-            routing_key=queue,
-            body=message)
+                          routing_key=queue,
+                          body=message)
     connection.close()
 
+def get_destination_route(dnet, routes):
+    """
+    Lookup the desination route
+    """
+    if dnet in routes:
+        dest_route = routes[dnet]
+    else:
+        dest_route = []
+    return dest_route
+
 def router(self, adjacent):
-    global done
-    print "Started: %d" % self
+    """
+    Top Level Router Thread
+    """
+    global DONE
+    print("Started: %d" % self)
     routes = {}
-    while not done:
+    while not DONE:
+        time.sleep(1)
         try:
             for msg in get_message(self):
                 jmsg = json.loads(msg)
-                if not in_hash(self,json.dumps(jmsg['message'])):
-                    drop = False
-                    jmsg['route'].append(self)
+                if in_hash(self, json.dumps(jmsg['message'])):
+                    continue
 
-                    # Learn shortest route
-                    if jmsg['snet'] in routes:
-                        if len(jmsg['route']) < len(routes[jmsg['snet']]):
-                            routes[jmsg['snet']] = jmsg['route'][::-1]
-                    else:
+                jmsg['route'].append(self)
+
+                # Learn shortest route
+                if jmsg['snet'] in routes:
+                    if len(jmsg['route']) < len(routes[jmsg['snet']]):
                         routes[jmsg['snet']] = jmsg['route'][::-1]
-
-                    # TODO: code to determine if the message should be dropped
-
-                    # Drop message
-                    if drop:
-                        continue
-
-                    # Determine destination route
-                    if jmsg['dnet'] in routes: 
-                        dest_route = routes[jmsg['dnet']]
-                    else:
-                        dest_route = []
-
-                    # Route/Deliver the Message
-                    msg = json.dumps(jmsg)
-                    if jmsg['dnet'] == self:
-                        # Deliver message
-                        print "%d: Delivered: %s" % (self,msg)
-                        send_message("n",jmsg['daddr'],msg)
-                    elif len(dest_route) > 1:
-                        # Route via learned route
-                        print "%d: Routing: %s" % (self,msg)
-                        send_message("q",dest_route[1],msg)
-                    else:
-                        # Broadcast
-                        print "%d: Broadcasting: %s" % (self,msg)
-                        for a in adjacent:
-                            send_message("q",a,msg)
                 else:
-                    pass
-                    #print "%d: %s" % (self, msg)
-                    #print "%d: Dropped" % self
-            else:
-                time.sleep(1)
+                    routes[jmsg['snet']] = jmsg['route'][::-1]
+
+
+                # Route/Deliver the Message
+                msg = json.dumps(jmsg)
+                if jmsg['dnet'] == self:
+                    # Deliver message
+                    print("%d: Delivered: %s" % (self, msg))
+                    send_message("n", jmsg['daddr'], msg)
+                    continue
+
+                dest_route = get_destination_route(jmsg['dnet'], routes)
+
+                if len(dest_route) > 1:
+                    # Route via learned route
+                    print("%d: Routing: %s" % (self, msg))
+                    send_message("q", dest_route[1], msg)
+                    continue
+
+                # Broadcast
+                print("%d: Broadcasting: %s" % (self, msg))
+                for each_node in adjacent:
+                    send_message("q", each_node, msg)
+
         except IOError:
             break
 
 ################################################################
 
-tlist = []
-hash_table.append({})
-for node in mesh.route_table:
-    t = threading.Thread(target=router,args=(node,mesh.route_table[node]))
-    hash_table.append({})
-    tlist.append(t)
+TLIST = []
+HASH_TABLE.append({})
+for node in MESH.route_table:
+    t = threading.Thread(target=router,
+                         args=(node, MESH.route_table[node]))
+    HASH_TABLE.append({})
+    TLIST.append(t)
     t.start()
 
-while not done:
+while not DONE:
     try:
         time.sleep(120)
     except KeyboardInterrupt:
-        done = True
+        DONE = True
 
-for t in tlist:
+for t in TLIST:
     t.join()
